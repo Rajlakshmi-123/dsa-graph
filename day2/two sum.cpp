8b6e68338fb6967e0e5aaa4128a8e4377758e79c
Raj Lakshmi <ac9754252@gmail.com>
2026-07-20 22:36:14 +0530

    two sum

---
+++ day2/two sum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int left = 0;
+    int right = numbers.size() - 1;
+
+    while (left < right) {
+        int sum = numbers[left] + numbers[right];
+
+        if (sum == target) {
+            return {left + 1, right + 1}; // 1-based indexing
+        }
+        else if (sum < target) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> numbers = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = twoSum(numbers, target);
+
+    if (!ans.empty()) {
+        cout << "Indices: " << ans[0] << " " << ans[1] << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
